eb6ce71d0384f425972613c18f4af3b3dbb63b0b
hinag <hinag@LAPTOP-2518S78U>
2016-08-04 08:17:02 -0400

    Add project files.

---
+++ AppNetA/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AppNetA.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
