d03ed14bebfd3a59947f8e089af923d2d8f52193
Divyansh <divyansh.sriv@gmail.com>
2016-08-06 15:31:54 -0400

    Revision 1. UI optimization

--- AppNetA/Models/PredictModel.cs
+++ AppNetA/Models/PredictModel.cs
@@ -11,21 +11,29 @@ namespace AppNetA.Models
        
             [Required]
         [Display(Name = "Area Square Id")]
+        [Range(38, 11454, ErrorMessage = "Area Square id should be between 38 and 11454. Refer to available square ids in that region.")]
         public int squareID { get; set; }
+
             [Required]
         [Display(Name = "Select Country")]
-        public int countryCode { get; set; }
+        public String countryCode { get; set; }
             [Required]
+
         [Display(Name = "Incoming SMS Activity")]
+        [Range(0, 27, ErrorMessage = "SMS in Activity should be between 0 and 27")]
         public float smsInActivity { get; set; }
             [Required]
         [Display(Name = "Outgoing SMS Activity")]
+        [Range(0, 20, ErrorMessage = "SMS out Activity should be between 0 and 20")]
         public float smsOutActivity { get; set; }
+
             [Required]
         [Display(Name = "Incoming Call Activity")]
+        [Range(0, 16, ErrorMessage = "Call in Activity should be between 0 and 16")]
         public float callInActivity { get; set; }
             [Required]
         [Display(Name = "Internet Traffic Activity")]
+        [Range(0, 70, ErrorMessage = "Internet Traffic Activity should be between 0 and 70")]
         public float internetTrafficActivity { get; set; }
             [Required]
         [Display(Name = "Date")]
@@ -33,9 +41,11 @@ namespace AppNetA.Models
         public DateTime date { get; set; }
         [Display(Name = "Hour")]
         [Required]
+        [Range(0, 23, ErrorMessage = "Hour should be betweeen 0 and 23")]
         public int hour { get; set; }
 
         [Display(Name = "Call Out Activity")]
+        [Range(0, 35, ErrorMessage = "Call out Activity should be between 0 and 35 as factor")]
         public float callOutActivity { get; set; }
 
         public string category { get; set; }
